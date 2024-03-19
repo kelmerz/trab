1acc6b6c05804f72102e48934c922d0618f2394c
mateusmacciel <78906680+mateusmacciel@users.noreply.github.com>
2024-03-19 20:45:20 -0300

    Update cadastrar.php

--- mavian/php/cadastrar.php
+++ mavian/php/cadastrar.php
@@ -4,14 +4,10 @@
     $cpf = $_POST["cpf"];
     $telefone = $_POST["telefone"];
 
-    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
-
-    $senha_hash_10 = mb_substr($senha_hash, 0, 20, 'UTF-8'); 
-
 
     $con = mysqli_connect("localhost:3306", "root", "PUC@1234", "mavian");
 
-    $query = "INSERT INTO usuario (email, senha, cpf, telefone) VALUES ('$email', '$senha_hash_10', '$cpf', '$telefone')";
+    $query = "INSERT INTO usuario (email, senha, cpf, telefone) VALUES ('$email', '$senha', '$cpf', '$telefone')";
 
     mysqli_query($con, $query);
 ?>
